a963c156dadc21fc66b87683735b9b652f56acc1
ejink0 <wls5819@naver.com>
2025-06-04 23:37:08 +0900

    [ADD] swagger관련 코드 커밋

---
+++ src/main/java/ask/anything/config/SecurityConfig.java
@@ -0,0 +1,83 @@
+package ask.anything.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.firewall.DefaultHttpFirewall;
+import org.springframework.security.web.firewall.HttpFirewall;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    //private final JwtTokenProvider jwtTokenProvider;
+    //private final CorsFilter       corsFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                        "/v3/api-docs/**",
+                        "/configuration/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/webjars/**",
+                        "/css/**",
+                        "/js/**",
+                        "/images/**",
+                        "/auth/signUp"
+                ).permitAll()
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
+
+
+    public HttpFirewall defaultHttpFirewall() {
+        return new DefaultHttpFirewall();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        JwtFilter jwtFilter = new JwtFilter(jwtTokenProvider);
+//
+//        http.csrf()
+//            .disable()
+//            .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+//            .exceptionHandling()
+//
+//            // 세션을 사용하지 않기 때문에 STATELESS로 설정
+//            .and()
+//            .sessionManagement()
+//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//
+//            .and()
+//            .authorizeRequests()
+//            //            .antMatchers("/login", "/auth/**")
+//            .antMatchers("/**")
+//            .permitAll()
+//            .antMatchers("/swagger-ui/**")
+//            .permitAll()
+//
+//            .anyRequest()
+//            .authenticated()
+//
+//            .and()
+//            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+//    }
+
+}
---
+++ src/main/java/ask/anything/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package ask.anything.config;
+
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    // SpringDoc에서는 이 어노테이션만으로 설정 가능
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new Components().addSecuritySchemes("BearerAuth",
+                        new SecurityScheme()
+                                .name("BearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ));
+    }
+}
